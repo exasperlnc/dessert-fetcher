81466f964067058b45fde88fec1319b464555a32
Logan Cole <emtlogancole@gmail.com>
2024-07-13 12:21:45 -0500

    Initial Commit

---
+++ dessert-fetcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dessert-fetcher
+//
+//  Created by Logan Cole on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dessert-fetcher/dessert_fetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  dessert_fetcherApp.swift
+//  dessert-fetcher
+//
+//  Created by Logan Cole on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct dessert_fetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
