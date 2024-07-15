945776596ba76ecdf657bb67c1b14b24c5b725dc
Logan Cole <emtlogancole@gmail.com>
2024-07-15 15:21:17 -0500

    Add Dessert and Recipe Models

--- Models/DessertsResponse.swift
+++ Models/DessertsResponse.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct DessertsResponse: Codable {
+    let meals: [Dessert]
+}
--- Models/RecipeResponse.swift
+++ Models/RecipeResponse.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct RecipeResponse: Codable {
+    let meals: [Recipe]
+}
