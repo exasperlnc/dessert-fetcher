854efd8cc51b46853b61e12f3d36bd896106e3e1
Logan Cole <emtlogancole@gmail.com>
2024-07-15 15:26:50 -0500

    add Dessert Model

--- Dessert.swift
+++ Dessert.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Dessert: Codable {
+    let idMeal: String
+    let strMeal: String
+    let strMealThumb: String
+    
+    enum CodingKeys: String, CodingKey {
+        case idMeal = "idMeal"
+        case strMeal = "strMeal"
+        case strMealThumb = "strMealThumb"
+    }
+}
