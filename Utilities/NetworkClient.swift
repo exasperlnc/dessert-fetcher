13348d5b12974f8347d61a08872af309d4885439
Logan Cole <emtlogancole@gmail.com>
2024-07-15 15:23:01 -0500

    add NetworkClient utility

--- Utilities/NetworkClient.swift
+++ Utilities/NetworkClient.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class NetworkClient {
+
+    func performRequest(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        
+        task.resume()
+    }
+}
