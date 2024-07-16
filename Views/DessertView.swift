62c3b56b6c851dd2adf395c8734239ec56804d14
Logan Cole <emtlogancole@gmail.com>
2024-07-15 19:53:32 -0500

    Update view to display Desserts

--- Views/DessertView.swift
+++ Views/DessertView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DessertView.swift
 //  dessert-fetcher
 //
 //  Created by Logan Cole on 7/13/24.
@@ -7,23 +7,27 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DessertView: View {
     
     @StateObject var viewModel = DessertListViewModel()
     
     var body: some View {
-        List {
-            ForEach(viewModel.desserts, id: \.idMeal) { dessert in
-                Text(dessert.strMeal)
+        if viewModel.isLoading {
+            ProgressView()
+        } else {
+            List {
+                ForEach(viewModel.desserts, id: \.idMeal) { dessert in
+                    Text(dessert.strMeal)
+                }
             }
-        }
-        .padding()
-        .task {
-            await viewModel.fetchDesserts()
+            .padding()
+            .task {
+                await viewModel.fetchDesserts()
+            }
         }
     }
 }
 
 #Preview {
-    ContentView()
+    DessertView()
 }
