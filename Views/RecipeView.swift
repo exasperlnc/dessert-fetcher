24838f68bd9f7e4934b1f2accb85d3a4a6c9099e
Logan Cole <emtlogancole@gmail.com>
2024-07-18 10:36:24 -0500

    add Back button to RecipeView

--- Views/RecipeView.swift
+++ Views/RecipeView.swift
@@ -44,6 +44,11 @@ struct RecipeView: View {
                 }
             }
             .navigationTitle(dessert.strMeal)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Back") {}
+                }
+            }
             .task {
                 await viewModel.fetchRecipe(idMeal: dessert.idMeal)
             }
